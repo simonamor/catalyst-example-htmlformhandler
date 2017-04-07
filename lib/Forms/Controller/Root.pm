83fda04a4bf40619d22fbe71d800bcc8016ef6ad
Simon Amor <simon@leaky.org>
2017-04-07 09:50:14 +0100

    Added an HTML::FormHandler form and tweaked styles

--- lib/Forms/Controller/Root.pm
+++ lib/Forms/Controller/Root.pm
@@ -2,6 +2,8 @@ package Forms::Controller::Root;
 use Moose;
 use namespace::autoclean;
 
+use HTML::FormHandler;
+
 BEGIN { extends 'Catalyst::Controller' }
 
 #
@@ -30,6 +32,47 @@ The root page (/)
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
+
+    my $form = HTML::FormHandler->new(
+        field_list => [
+            name => {
+                do_wrapper => 0,
+                type => "Text",
+                required => 1,
+                messages => { required => 'You must enter a name' },
+                tags => {
+                    after_element => '<br>'
+                },
+            },
+            postcode => {
+                do_wrapper => 0,
+                type => "Text",
+                required => 1,
+                messages => { required => 'You must enter a postcode' },
+                tags => {
+                    after_element => '<br>'
+                },
+            },
+            email => {
+                do_wrapper => 0,
+                type => "Email",
+                required => 1,
+                tags => {
+                    after_element => '<br>'
+                },
+            },
+            submit => {
+                do_wrapper => 0,
+                type => "Submit",
+                value => "Submit"
+            },
+        ],
+    );
+
+    my $p = $c->request->params;
+
+    $form->process( params => $p, );
+    $c->stash( form => $form );
 
     $c->stash( template => "index.html" );
 }
